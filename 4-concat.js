2c06e41084cccd92aabd0141d413669160d9d80a
Tzienom <tzienom@gmail.com>
2025-05-27 16:50:20 +0100

    feat: updated 4-concat.js to format arguments to include 'is' in sentence.

--- 4-concat.js
+++ 4-concat.js
@@ -1,7 +1,16 @@
-function concatSentence(word1, word2) {
-  return word1 + " is " + word2;
+function concatSentence() {
+  let args = process.argv.slice(2);
+
+  if (args || !args) {
+    let firstWord;
+    let secondWord;
+
+    for (let i = 0; i < args.length; i++) {
+      firstWord = args[0];
+      secondWord = args[1];
+    }
+    console.log(`${firstWord} is ${secondWord}`);
+  }
 }
 
-console.log(concatSentence("c", "cool"));
-console.log(concatSentence());
-console.log(concatSentence("c"));
+concatSentence();
