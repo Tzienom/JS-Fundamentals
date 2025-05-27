5a840500125a58fe038e0f3579a8012303a47854
Tzienom <tzienom@gmail.com>
2025-05-27 01:26:33 +0100

    feat: updated 2-arguments.js to use console.log for output

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,9 +1,9 @@
 function arguments(...args) {
-  if (args.length === 0) return "No argument";
-  else if (args.length === 1) return "Argument found";
-  else return "Arguments found";
+  if (args.length === 0) console.log("No argument");
+  else if (args.length === 1) console.log("Argument found");
+  else console.log("Arguments found");
 }
 
-console.log(arguments("Argumment", "Another Argument"));
-console.log(arguments("Argument"));
-console.log(arguments());
+arguments("Argumment", "Another Argument", "A third Argument");
+arguments("Argument");
+arguments();
