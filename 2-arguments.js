4e36effaf68789d6cb43977c5d77d1a9895767de
Tzienom <tzienom@gmail.com>
2025-05-27 13:12:32 +0100

    feat: updated printArguments function

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,13 +1,11 @@
 function printArguments() {
   let args = process.argv.slice(2);
 
-  if (args.length === 0) console.log("No argument");
-  else {
-    for (let i = 0; i < args.length; i++) {
-      if (args.length === 1) console.log("Argument found");
-      else console.log("Arguments found");
-    }
-  }
+  if (args.length > 1) {
+    console.log("Arguments found");
+    for (let i = 0; i < args.length; i++);
+  } else if (args.length === 1) console.log("Argument found");
+  else console.log("No argument");
 }
 
 printArguments();
